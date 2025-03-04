b48d60f0e67cec34f0d4340c5cea8220afb5cf59
Chelsey Machin <chelseymachin@gmail.com>
2025-03-04 12:49:11 -0800

    build error fixes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
 import Profile from '../views/Profile.vue'
 import AuthCallback from '../views/AuthCallback.vue'
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/login', component: Login },
   { path: '/profile', component: Profile },
   { path: '/auth/callback', component: AuthCallback },
 ]
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -7,7 +7,7 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { fetchCurrentUser } from '@/composables/useRavelryAPI'
+import { fetchCurrentUser } from '@/composables/useRavelryApi'
 import { useAuthStore } from '@/stores/auth'
 import type { RavelryUser } from '@/types/ravelry'
 import router from '@/router'
